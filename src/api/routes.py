1ae5a3942214844022ecc81b49bca0c15ea6b555
blancojohn <johnmario.bdc@gmail.com>
2024-07-02 15:58:06 -0400

    Creación y validación de ruta login con JWT OK

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,11 +1,13 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import datetime
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, Favorite_Planet, Favorite_People, People, Planet, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -187,6 +189,39 @@ def register_user():
     db.session.commit()
 
     return jsonify({"success": "Registro satisfactorio. Por favor hacer click en inicia sesión"}), 200
+
+@api.route('/login', methods=['POST'])
+def login_user():
+
+    print(request.get_json())
+    email= request.json.get('email')
+    password= request.json.get('password')
+
+    #Validadciones:
+    if not email:
+        return jsonify({"messagge": "Email es requerido."}), 400
+    
+    if not password:
+        return jsonify({"messagge": "Password es requerido."}), 400
+    
+    #Valida si el email y password son incorrectos.
+    user= User.query.filter_by(email= email).first()
+    if not user:
+        return jsonify({"messagge": "Email/Password son incorrectos."}), 401
+    
+    if not check_password_hash(user.password, password):
+         return jsonify({"messagge": "Email/Password son incorrectos"}), 401
+    
+    expirate_token = datetime.timedelta(days = 1)
+    access_token = create_access_token(identity = user.id, expires_delta = expirate_token)
+
+    datos = {
+        "success": "Inicio de sesión exitoso",
+        "access_token":access_token,
+        "user": user.to_dict()
+    }
+
+    return jsonify(datos), 200
 
 @api.route('/users', methods=['GET'])
 def get_users():
